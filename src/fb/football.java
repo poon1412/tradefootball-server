f3e7e9ec8ea23533f7e657f50e94132351017328
MSI\Poon <poon1412@gmail.com>
2017-09-12 13:48:59 +0700

    TEST

---
+++ src/fb/football.java
@@ -0,0 +1,17 @@
+package fb;
+import javax.jws.WebParam;
+import javax.ws.rs.FormParam; 
+import javax.ws.rs.GET; 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces; 
+import javax.ws.rs.core.MediaType;
+public class football {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TEST");
+	}
+
+}
