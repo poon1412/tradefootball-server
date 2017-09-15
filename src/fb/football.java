0608457f3beaa89d7b7d3bc4b2d7f77a70e992f2
Linlijian <Linlijian@172.18.133.186>
2017-09-16 03:40:55 +0700

    delete element "address" and add path "position"

--- src/fb/football.java
+++ src/fb/football.java
@@ -41,7 +41,6 @@ public class football {
 				xml += "<weight>"+object.get("weight").toString()+"</weight>";
 				xml += "<height>"+object.get("height").toString()+"</height>";
 				xml += "<D_M_Y>"+object.get("D_M_Y").toString()+"</D_M_Y>";
-				xml += "<address>"+object.get("address").toString()+"</address>";
 				xml += "<team>"+object.get("team").toString()+"</team>";
 				xml += "<position>"+object.get("position").toString()+"</position>";
 			xml += "</player>";
@@ -53,5 +52,38 @@ public class football {
 		return xml;  
 	}
 	
-	
+	@GET
+	@Path("/findposition/{name}") 
+	@Produces(MediaType.TEXT_XML)  
+	public String findposition(@PathParam("nameposition") String nameposition) {
+		DB db = new Connect().mongo();
+		DBCollection table = db.getCollection("player");
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("nameposition", nameposition);
+		
+		DBCursor cursor = table.find(searchQuery);
+		List<DBObject> myList = cursor.toArray();
+		
+		String xml = "<?xml version=\"1.0\"?>";
+		xml += "<return>";
+		for (DBObject object : myList) {
+			xml += "<player>";
+				xml += "<number>"+object.get("number").toString()+"</number>";
+				xml += "<name>"+object.get("name").toString()+"</name>";
+				xml += "<lname>"+object.get("lname").toString()+"</lname>";
+				xml += "<age>"+object.get("age").toString()+"</age>";
+				xml += "<weight>"+object.get("weight").toString()+"</weight>";
+				xml += "<height>"+object.get("height").toString()+"</height>";
+				xml += "<D_M_Y>"+object.get("D_M_Y").toString()+"</D_M_Y>";
+				xml += "<team>"+object.get("team").toString()+"</team>";
+				xml += "<position>"+object.get("position").toString()+"</position>";
+			xml += "</player>";
+		}
+		xml += "</return>";
+		
+		System.out.println(xml);
+		
+		return xml;  
+	}
 }
